1c6d892530539ee0ba271297e22c640b39d045de
Witrman <32022781+Witrman@users.noreply.github.com>
2018-02-04 19:52:06 +0400

    Initial commit

---
+++ src/Dish.java
@@ -0,0 +1,41 @@
+public class Dish {
+   double cost;
+   String name, desciption;
+
+    Dish(String name, String description)
+   { this.name = name;
+   this.desciption = description;
+   cost = 0;
+   }
+
+    Dish(String name, String description, double cost)
+    {
+        this(name,description);
+        this.cost= cost;
+    }
+
+     String getName() {
+        return name;
+    }
+
+     double getCost() {
+        return cost;
+    }
+
+     String getDesciption() {
+        return desciption;
+    }
+
+     void setName(String name) {
+        this.name = name;
+    }
+
+     void setCost(double cost) {
+        this.cost = cost;
+    }
+
+     void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+}
---
+++ src/Order.java
@@ -0,0 +1,161 @@
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Order {
+    int size;
+    Dish[] dishes;
+
+    Order() {
+        dishes = new Dish[16];
+    }
+
+    Order(int size) {
+        dishes = new Dish[size];
+    }
+
+    Order(Dish[] dishes) {
+        this.dishes = dishes;
+    }
+
+    boolean add(Dish dish) {
+        for (int i = 0; i < dishes.length; i++) {
+            if (i==dishes.length-1 && dishes[i]!=null) {doubleOrderDishes();}
+            if (dishes[i] == null) {
+                dishes[i] = dish;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void doubleOrderDishes()
+    {
+        Dish[] dishes1 = new Dish[dishes.length*2];
+        for (int i = 0; i < dishes.length; i++) {
+                dishes1[i] = dishes[i];
+
+        }
+        dishes=dishes1;
+
+    }
+
+    boolean remove(String dishName) {
+        for (int i = 0; i < dishes.length; i++) {
+            if (Objects.equals(dishes[i].getName(), dishName)) {
+                for (int j = i; j < dishes.length-1 ; j++) {
+                    dishes[j]=dishes[j+1];
+                    if (j==dishes.length-1){dishes[j+1]=null;}
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int removeAll(String dishName) {
+        int count = 0;
+        for (int i = 0; i < dishes.length; i++) {
+            if (dishes[i] != null && Objects.equals(dishes[i].getName(), dishName)) {
+                for (int j = i; j < dishes.length-1 ; j++) {
+                    dishes[j]=dishes[j+1];
+                    if (j==dishes.length-1){dishes[j+1]=null;}
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int dishQuantity()
+    {
+        int count = 0;
+        for (Dish dish : dishes) {
+            if (dish != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int dishQuantity(String dishName)
+    {
+        int count = 0;
+        for (Dish dish : dishes) {
+            if (dish != null && Objects.equals(dish.getName(), dishName)) {
+            count++;
+            }
+        }
+        return count;
+    }
+
+    Dish[] getDishes() {
+        Dish[] bdishes = new Dish[dishQuantity()];
+        for (int i = 0; i < bdishes.length; i++) {
+            bdishes[i]=dishes[i];
+        }
+        return bdishes;
+    }
+
+    double costTotal()
+    {   double sum = 0;
+        for (Dish dish : dishes) {
+            if (dish != null) {
+                sum+= dish.cost;
+            }
+        }
+        return sum;
+    }
+
+    String[] dishesNames()
+    {
+        int h=0;
+        String[] dishNames = new String[dishQuantity()];
+        for (int i =0  ; i < dishNames.length-1;i++  ) {
+            for (int j = 0; j <dishNames.length && i<24 ; j++) {
+                if(dishNames[j]==dishes[i].getName())
+                { i++;
+                j=0;}
+            }
+            if(i==dishQuantity())continue;
+            dishNames[h] = dishes[i].getName();
+            h++;
+        }
+        String[] dishNamesRezult = new String[h];
+        for (int i = 0; i <dishNamesRezult.length ; i++) {
+            dishNamesRezult[i]=dishNames[i];
+        }
+        return dishNamesRezult;
+    }
+
+Dish[] sortedDishesByCostDesc()
+{    Dish bufDish;
+    for (int i = 0; i <  dishQuantity()-1; i++) {
+        for (int j = 0; j < dishQuantity()-1; j++) {
+             if (dishes[j].getCost()>=dishes[j+1].getCost())
+             {
+                 bufDish = dishes[j];
+                 dishes[j] = dishes[j+1];
+                 dishes[j+1]=bufDish;
+             }
+        }
+    }
+    return getDishes();
+}
+
+
+
+    void pr()
+     {
+         for (int i = 0; i < dishes.length; i++) {
+             if (dishes[i] != null)
+             {
+                 System.out.println(i+"    "+dishes[i].getName());
+             }
+         }
+
+     }
+
+
+}
